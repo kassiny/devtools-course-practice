35884430123a60c4c1570834ae7508c8398f1696
Masha <razumovahome@mail.ru>
2018-05-31 17:35:56 +0300

    Changed .h file

--- modules/check_line_segment_cross/include/lineSegment.h
+++ modules/check_line_segment_cross/include/lineSegment.h
@@ -10,12 +10,15 @@ public:
     LineSegment(double _x1, double _y1, double _x2, double _y2);
     LineSegment (const LineSegment&);
     LineSegment& operator=(const LineSegment&);
-    bool isCross (const LineSegment&) const;
+    bool operator==(const LineSegment& line) const;
+    bool isCross (LineSegment&);
     bool isCross(double x1, double y1, double x2, double y2) const;
     double scalarProduct(const LineSegment& line);
     double vectorProduct(const LineSegment& line);
     ~LineSegment();
+    
 private:
+    int sign(double _x1, double _y1);
     double x1, y1, x2, y2;
 
 };

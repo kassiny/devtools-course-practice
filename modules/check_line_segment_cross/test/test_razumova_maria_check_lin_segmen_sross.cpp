bf8c059cf2b10e78d16065bb02c24ead3db88c42
Masha <razumovahome@mail.ru>
2018-05-31 17:37:33 +0300

    some tests

--- modules/check_line_segment_cross/test/test_razumova_maria_check_lin_segmen_sross.cpp
+++ modules/check_line_segment_cross/test/test_razumova_maria_check_lin_segmen_sross.cpp
@@ -11,6 +11,7 @@ TEST(Razumova_Maria_LineCross, Can_create_LineSegment_without_parametrs)
     //AAA
     ASSERT_NO_THROW(LineSegment());
 }
+
 TEST(Razumova_Maria_LineCross,Can_create_LineSegment_with_parametrs)
 {
     //Arrange
@@ -25,3 +26,65 @@ TEST(Razumova_Maria_LineCross,Can_create_LineSegment_with_parametrs)
     //Assert
     ASSERT_NO_THROW(LineSegment(x1,y1,x2,y2));
 }
+
+TEST (Razumova_Maria_LineCross, Can_create_via_copyng)
+{
+    //Arrange
+    LineSegment line(1,2,3,4);
+
+    //Act
+    LineSegment line2(line);
+    //Assert
+    EXPECT_EQ(line, line2);
+}
+
+TEST(Razumova_Maria_LineCross, Can_compare_equal_lines)
+{
+    //Arrange
+    LineSegment line1(1,0,0,1);
+    LineSegment line2(1,0,0,1);
+    
+    //Act && Assert
+    EXPECT_TRUE(line1 == line2);
+}
+
+TEST(Razumova_Maria_LineCross, Can_compare_not_equal_lines)
+{
+    //Arrange
+    LineSegment line1(1,0,0,1);
+    LineSegment line2(1,0,1,1);
+    
+    //Act && Assert
+    EXPECT_FALSE(line1 == line2);
+}
+
+ TEST (Razumova_Maria_LineCross, Can_assign_different_lines)
+ {
+     //Arrange
+     LineSegment line1(1,1,3,4);
+     LineSegment line2(2,2,2,2);
+     //Act
+     line2 = line1;
+     //Assert
+     EXPECT_EQ(line1, line2);
+ }
+
+ TEST (Razumova_Maria_LineCross, Can_assign_equal_lines)
+ {
+     //Arrange
+     LineSegment line1(1,1,3,4);
+     LineSegment line2(2,2,2,2);
+     //Act
+     line2 = line1;
+     //Assert
+     EXPECT_EQ(line1, line2);
+ }
+
+ TEST (Razumova_Maria_LineCross, Can_assign_line_to_itself)
+ {
+     //Arrange
+     LineSegment line(1,1,1,1);
+
+     //Act & Assert
+     ASSERT_NO_THROW(line = line);
+ }
